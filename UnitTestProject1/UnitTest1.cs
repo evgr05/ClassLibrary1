a19760b9171ecee65aca45146ba53ca595eafb80
Pluf <zelux228@mail.ru>
2024-03-13 14:31:57 +0300

    1

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,18 +1,41 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using ClassLibrary1;
+
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestClass]
+        public class BiblTests
         {
-            int a = 10;
-            int b = 5;
-            int c = 8;
+            [TestMethod]
+            public void TestMinWhenFirstIsMin()
+            {
+                int result = Bibl.Min(2, 5, 7);
+                Assert.AreEqual(2, result);
+            }
 
+            [TestMethod]
+            public void TestMinWhenSecondIsMin()
+            {
+                int result = Bibl.Min(8, 4, 10);
+                Assert.AreEqual(4, result);
+            }
+
+            [TestMethod]
+            public void TestMinWhenThirdIsMin()
+            {
+                int result = Bibl.Min(6, 9, 3);
+                Assert.AreEqual(3, result);
+            }
+            [TestMethod]
+            public void TestNoMin()
+            {
+                int result = Bibl.Min(2, 5, 7);
+                Assert.AreEqual(7, result);
+            }
         }
     }
 }
